170fbe079ad0edbe13dee067a6ea2c6d4aadb9a2
Dave Seidman <daveseidman@gmail.com>
2026-04-21 08:36:01 -0400

    basename and other updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_PUBLIC_BASE || '/',
+  server: {
+    host: true
+  },
   css: {
     preprocessorOptions: {
       scss: {
